d356004921386e95b2f090200521f78649ba7322
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-06-28 19:15:09 +0100

    update new file

--- src/Component/Navber.jsx
+++ src/Component/Navber.jsx
@@ -20,32 +20,32 @@ const Navber = () => {
 
     const SignUPButton = []
   return (
-    <div className=' relative' >
-              <nav className=' fixed bg-white/90 border-gray-100 top-0 right-0 left-0 z-50 border-b'>
-          <div className=' w-full container  items-center mx-auto flex  justify-between px-4 sm:px-6 lg:px-8 h-20 md:h20 gap-4'>
-                <div className=' flex items-center gap-1'>
-                       <img src={Mentoreelogo} alt="" />
+    <div className='' >
+              <nav className=' fixed bg-white/90 border-gray-200 top-0 right-0 left-0 z-50 border-b'>
+          <div className=' w-full container  items-center mx-auto flex  justify-between px-4 sm:px-6 lg:px-8 h-25 md:h20 gap-4'>
+                <div  className='flex-shrink-0 w-32'>
+                       <img src={Mentoreelogo} alt="" className=' w-full object-contain' />
                 </div>
                 <div>
                       
                 </div>
-                <div className=' flex justify-between gap-8'>
-                      <button onClick={ () => setIsmenuOpen(!isMenuopen) } className=' md:hidden p-2' >
+                <div className=' flex justify-between '>
+                      <button onClick={ () => setIsmenuOpen(!isMenuopen) } className=' lg:hidden p-2  bg-blue-400 rounded-full ' >
                                 {
-                                    isMenuopen ? <HiX className=' size-6' />  : <HiMenu />
+                                    isMenuopen ? <HiX  className=' size-8' />  : <HiMenu className=' size-8' />
                                 }
                       </button>
 
-                      <div className='hidden items-center md:flex gap-10'>
+                         <div className='hidden items-center  lg:flex gap-10'>
                             {
                               navLink.map((link, index) => (
                                   <a key={index} href={link.href}
                                    onClick={() => setActivelink(link.href)} 
-                                   className=' text-2xl font-bold hover:text-blue-400'
+                                   className=' text-2xl font-bold  hover:text-blue-400'
                                   >{link.label} </a>
                               ))
                             }
-                             <div className=' flex justify-between gap-8'>
+                             <div className=' flex justify-between gap-4'>
                             <button className=' border rounded-full text-black hover:bg-blue-400 hover:text-white  p-4 w-40'>SIGN UP</button>
                             <button className='text-white bg-blue-400 rounded-full p-4 w-40 hover:bg-white hover:text-black border'>LOG IN</button>
                       </div>
@@ -55,17 +55,19 @@ const Navber = () => {
     </nav>
     
 
-                <div className=' w-full container pt-20 '>
+                <div className=' w-full container pt-30  '>
                {
-                         isMenuopen && (
-                            <div className=' flex flex-col gap-4 md:hidden border-b border-gray-300  pb-5'>
+                        isMenuopen && (
+                           <div className='flex flex-col  gap-4 lg:hidden border-b z-50 border-gray-300 pb-5 left-0 right-0'>
                                         {navLink.map((link, index) => (
                                           <a key={index} href={link.href} onClick={() => {
                                             setActivelink(link.href);
                                             setIsmenuOpen(false);
                                           }}
-                                           className=' text-2xl font-semibold flex justify-between  '
-                                          >{link.label} <HiArrowRight className=' text-gray-700 size-6' /> </a>
+                                           className=' text-2xl NavText  flex justify-between  hover:text-blue-400 hover:scale-105
+                                            transition-transform duration-200 '
+                                          >  <span className='ml-7' >{link.label}</span>
+                                             <HiArrowRight className=' text-gray-700 size-6 mr-5' /> </a>
                                         ))}
 
                                         { subNavlink.map((link, index) => (
@@ -73,12 +75,12 @@ const Navber = () => {
                                             onClick={() => {setActivelink(link.href);
                                               setIsmenuOpen(false);
                                             }}
-                                            className=' text-xl text-gray-500'
+                                            className=' ml-7 text-xl text-gray-500 font-semibold hover:text-blue-400 '
                                           >{link.label}</a>
                                         ))}
                                    <div className='flex gap-4 justify-end'>
-                            <button className=' border rounded-full text-black  p-4 w-40'>SIGN UP</button>
-                            <button className='text-white bg-blue-400 rounded-full p-4 w-40'>LOG IN</button>
+                            <button className=' border rounded-full text-black hover:bg-blue-400 hover:text-white  p-4 w-40'>SIGN UP</button>
+                            <button className='text-white bg-blue-400 rounded-full p-4 w-40 hover:bg-white hover:text-black border'>LOG IN</button>
                       </div>
                             </div>
                          )
