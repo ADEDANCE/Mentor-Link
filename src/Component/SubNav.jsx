8e6d2e849f3c909a93ac82065ac8716ad91e1b66
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-06-28 19:13:48 +0100

    Create new file

---
+++ src/Component/SubNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubNav = () => {
+  return (
+    <section className=' fixed bg-white/90 border-gray-200 border-b z-50 left-0 right-0'>SubNav</section>
+  )
+}
+
+export default SubNav
